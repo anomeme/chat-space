db24ca04f63b19cf356f7201ce74a1fe43ba93d0
anomeme <x.music.sky@gmail.com>
2019-10-16 22:26:02 +0900

    key_change

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/anomusic.pem']
+                  keys: ['~/.ssh/id_rsa.pub']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
